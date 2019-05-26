fdd5a05b6c042b4ee9b10f2d8ddfb8b323f4f339
Your Name <you@example.com>
2019-05-26 18:10:00 +0800

    魏烈聪倒数第二次

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,8 @@ module.exports = {
     devServer:{
         proxy:{
             "/api":{
-<<<<<<< HEAD
                 target:"http://tg.wochu.cn",
-=======
                 target:"http://api9.wochu.cn",
->>>>>>> dfdc5f52016d31742d51cddbfd7817d5c235e842
                 changeOrigin:true,
                 pathRewrite:{
                     "^/api":""
